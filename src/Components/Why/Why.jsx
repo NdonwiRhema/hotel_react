c1a80371b5534e0fee21c2ab65d57e76fd9198d1
Ndonwi Rhema <rhemandonwi59@gmail.com>
2023-03-01 18:25:11 +0530

    Why Choose Us 🔇

--- src/Components/Why/Why.jsx
+++ src/Components/Why/Why.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
+import "./Why.css"
 import Whyimg from '../../images/Image1.jpg'
 const Why = () => {
   return (
     <>
         <section>
-            <div className='why_choose container-fluid'>
+            <div className='why_choose'>
                 <div>
-                    <div className='img'>
+                    <div className='imgholder'>
                         <img src={Whyimg} alt='' />
                     </div>
+                    <div className='headerOverlay'></div>
                 </div>
+             <div className='whyContent'>
+                 <h4>We Provide Everything You Need</h4>
+            </div>
             </div>
         </section>
     </>
+  
   )
 }
 
